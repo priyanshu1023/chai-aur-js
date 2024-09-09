15c3d62d9cdffeb0eb7ca90dc385a525ab99ba1e
priyanshu1023 <priyanshu1023g@gmail.com>
2024-09-09 10:34:46 +0530

    string

---
+++ 01basic/5string.js
@@ -0,0 +1,11 @@
+const gamenew= "priyanshu"
+console.log( gamenew.indexOf("i"));
+
+console.log(gamenew.substring(0,3)); //=> yeh substring dega positive me  
+
+console.log(gamenew.slice(-8,4));  //=> yeh negative bhi le lega 
+
+
+const newstringOne ="  priyanshu  "
+console.log(newstringOne);
+console.log(newstringOne.trim()); //=> yeh string kii faltu jagah hata dega
